86628b3a1d489396ed160e6d818279b1129a600c
Samuel Chow <samuel.chow.45@gmail.com>
2025-07-19 10:10:49 -0400

    update celery settings

--- autograder/settings.py
+++ autograder/settings.py
@@ -217,7 +217,14 @@ SOCIAL_AUTH_PIPELINE = (
 
 
 CELERY_TIMEZONE = "America/New_York"
-CELERY_BROKER_URL = "redis://redis:6379/0" if DEBUG else "redis://localhost:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0" if DEBUG else "redis://127.0.0.1:6379"
 
 CELERY_ACCEPT_CONTENT = ["json", "pickle"]
 CELERY_TASK_SERIALIZER = "pickle"
+
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+CELERY_RESULT_EXTENDED = True
+CELERY_worker_state_db = True
+CELERY_result_persistent = True
+CELERY_RESULT_BACKEND = 'django-db'
