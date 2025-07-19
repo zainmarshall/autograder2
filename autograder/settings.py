8c728dfaaf322fc04d49c9c9f30f534976757390
Samuel Chow <samuel.chow.45@gmail.com>
2025-07-19 10:15:40 -0400

    move debug setting to .env

--- autograder/settings.py
+++ autograder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = ["tjctgrader.org", "localhost", "127.0.0.1"]
 
