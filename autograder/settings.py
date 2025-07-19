36beef66138e51aadc2f926a732b62b32674b0f8
Samuel Chow <samuel.chow.45@gmail.com>
2025-07-19 10:19:58 -0400

    change bool

--- autograder/settings.py
+++ autograder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(config("DEBUG"))
+DEBUG = config("DEBUG") == 'True'
 
 ALLOWED_HOSTS = ["tjctgrader.org", "localhost", "127.0.0.1"]
 
