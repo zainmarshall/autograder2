d05a20d5fd7f05aa4389bef96d41ddd087b65f88
Samuel Chow <samuel.chow.45@gmail.com>
2025-07-19 10:18:12 -0400

    facepalm

--- autograder/settings.py
+++ autograder/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = bool(config("SECRET_KEY"))
+SECRET_KEY = config("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = bool(config("DEBUG"))
 
 ALLOWED_HOSTS = ["tjctgrader.org", "localhost", "127.0.0.1"]
 
