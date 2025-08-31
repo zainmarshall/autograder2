bddbbdd6e5ba690665809a37c7228bff66a1adac
Zain Marshall <zainmarshall1000@gmail.com>
2025-08-31 07:28:48 -0400

    submissions api

--- autograder/api/urls.py
+++ autograder/api/urls.py
@@ -3,6 +3,7 @@ from .problem_list_api import ProblemListAPI
 from .problem_detail_api import ProblemDetailAPI
 from .rankings_api import RankingsAPI
 from .contest_api import ContestListAPI, ContestDetailAPI
+from .submission_list_api import SubmissionListAPI
 
 urlpatterns = [
     path("problems/", ProblemListAPI.as_view(), name="api_problem_list"),
@@ -10,4 +11,5 @@ urlpatterns = [
     path("rankings/<int:season>/", RankingsAPI.as_view(), name="api_rankings"),
     path("contests/", ContestListAPI.as_view(), name="api_contest_list"),
     path("contests/<int:cid>/", ContestDetailAPI.as_view(), name="api_contest_detail"),
+    path("submissions/", SubmissionListAPI.as_view(), name="api_submission_list"),
 ]
